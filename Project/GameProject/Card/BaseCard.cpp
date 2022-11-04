3898f2ab7c3beef09b48474d35e85eae1eb67ca3
Ramiru0001 <shian2528@gmail.com>
2022-11-04 10:13:49 +0900

    a

--- Project/GameProject/Card/BaseCard.cpp
+++ Project/GameProject/Card/BaseCard.cpp
@@ -32,7 +32,7 @@ void BaseCard::ImageSet() {
 		diamond[i].SetSize(271, 406);
 		club[i].SetSize(271, 406);
 		spade[i].SetSize(271, 406);
-	}
+	}//カードの後ろの画像のセット
 	BehindCard = COPY_RESOURCE("TrumpBehind", CImage);
 	BehindCard.SetRect(23, 23, 294, 429);
 	BehindCard.SetSize(271, 406);
@@ -44,12 +44,9 @@ void BaseCard::Draw() {
 	spade[0].Draw();*/
 }
 void BaseCard::CardListSet() {
-	for (int i = 0; i++; i < 52) {
-		AllCard_list.push_front(CardNum[i]);
-		Remaining_list.push_front(CardNum[i]);
-	}
-	while (!(Remaining_list.empty())) {
-		std::cout << Remaining_list.size() << std::endl;
+	//std::cout << Remaining_list.size() << std::endl;
+	while (!Remaining_list.empty()) {
+		//std::cout << Remaining_list.size() << std::endl;
 		//残っているカードの数
 		int RemainingCard_num = Remaining_list.size();
 		//残っているカードの数の中で、乱数を生成する関数を作成。
@@ -68,21 +65,42 @@ void BaseCard::CardListSet() {
 			Reserve_list0.push_front(NextProcessCardNum);
 			break;
 		case 51:
+		case 50:
 			Reserve_list1.push_front(NextProcessCardNum);
 			break;
 		case 49:
+		case 48:
+		case 47:
 			Reserve_list2.push_front(NextProcessCardNum);
 			break;
 		case 46:
+		case 45:
+		case 44:
+		case 43:
 			Reserve_list3.push_front(NextProcessCardNum);
 			break;
 		case 42:
+		case 41:
+		case 40:
+		case 39:
+		case 38:
 			Reserve_list4.push_front(NextProcessCardNum);
 			break;
 		case 37:
+		case 36:
+		case 35:
+		case 34:
+		case 33:
+		case 32:
 			Reserve_list5.push_front(NextProcessCardNum);
 			break;
 		case 31:
+		case 30:
+		case 29:
+		case 28:
+		case 27:
+		case 26:
+		case 25:
 			Reserve_list6.push_front(NextProcessCardNum);
 			break;
 		default:
@@ -90,4 +108,41 @@ void BaseCard::CardListSet() {
 			break;
 		}
 	}
+	//std::cout << Remaining_list.size() << std::endl;
+	std::cout << "Stock :";
+	for (auto const& i : Stock_list) {
+		std::cout << "," << i ;
+	}
+	std::cout <<std::endl<< " Reserve_list0 :" ; 
+	for (auto const& i : Reserve_list0) {
+		std::cout << "," << i;
+	}
+	std::cout << std::endl << " Reserve_list1 :" ;
+	for (auto const& i : Reserve_list1) {
+		std::cout << "," << i;
+	}
+	std::cout << std::endl << " Reserve_list2 :" ;
+	for (auto const& i : Reserve_list2) {
+		std::cout << "," << i;
+	}
+	std::cout << std::endl << " Reserve_list3 :" ;
+	for (auto const& i : Reserve_list3) {
+		std::cout << "," << i;
+	}
+	std::cout << std::endl << " Reserve_list4 :" ;
+	for (auto const& i : Reserve_list4) {
+		std::cout << "," << i;
+	}
+	std::cout << std::endl << " Reserve_list5 :" ;
+	for (auto const& i : Reserve_list5) {
+		std::cout << "," << i;
+	}
+	std::cout << std::endl << " Reserve_list6 :" ;
+	for (auto const& i : Reserve_list6) {
+		std::cout << "," << i;
+	}
+	std::cout << std::endl << " Foundation_list :" ;
+	/*for (auto const& i : Foundation_list) {
+		std::cout << "," << i;
+	}*/
 }
